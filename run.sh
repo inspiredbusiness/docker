f5388b9f1f0604c1b8aef5120400d9fab547880e
inspiredbusiness <chris@inspiredbusiness.com.au>
2015-03-09 11:19:21 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,4 +23,4 @@ set_config "admin_passwd" $ENV_MASTER_PASSWORD
 set_config "addons_path" $ADDONS_PATH
 
 # start Odoo
-exec gosu odoo /usr/bin/openerp-server --config $CONFIG_FILE
+sudo odoo /usr/bin/openerp-server --config $CONFIG_FILE
